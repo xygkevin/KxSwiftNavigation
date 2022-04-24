4ec4159f257d6b24169937bfe7fbfaa05b901fa7
许亚光 <xu_yaguang@163.com>
2022-04-24 17:22:11 +0800

    no message

---
+++ SwiftDemo/Pods/Target Support Files/KxSwiftNavigation/KxSwiftNavigation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KxSwiftNavigation : NSObject
+@end
+@implementation PodsDummy_KxSwiftNavigation
+@end
---
+++ SwiftDemo/SwiftDemo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftDemo
+//
+//  Created by 许亚光 on 2022/4/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = NavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    
+
+}
+
